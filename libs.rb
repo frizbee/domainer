b25a741cbafb07cee12ffea673c154078a40f31f
AndreAntonov <mail2andrewa@gmail.com>
2016-07-25 14:19:05 +0700

    Create libs.rb

---
+++ libs.rb
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env ruby
+
+require 'zip'
+
+def extract_zip(file, destination)
+  FileUtils.mkdir_p(destination)
+  Zip::ZipFile.open(file) do |zip_file|
+    zip_file.each do |f|
+      fpath = File.join(destination,f.name)
+      zip_file.extract(f,fpath) unless File.exist?(fpath)
+    end
+  end
+end
+~
